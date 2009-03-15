80e87922bbb5da248bff77e1ca2baf7f3990055d
Jason Dunsmore <jason@dunsmor.com>
2009-03-14 21:44:51 -0500

    renamed and moved files.  rewrote most of quicksnips script.

--- conf/quicksnips-keys.jl
+++ conf/quicksnips-keys.jl
@@ -1,18 +1,18 @@
 (define quicksnips-keymap (make-keymap))
 (bind-keys global-keymap
-	   "M-S-s" (lambda () (system "~/quicksnips/bin/quicksnips -e rhel &"))
-	   "M-S-d" (lambda () (system "~/quicksnips/bin/quicksnips -e debian &"))
+	   "M-S-s" (lambda () (system "quicksnips -e &"))
+	   "M-S-d" (lambda () (system "quicksnips -e debian &"))
 	   "C-z" quicksnips-keymap)
 
-(bind-keys quicksnips-keymap "o" (lambda () (system "~/quicksnips/bin/quicksnips rhel server-overview")))
-(bind-keys quicksnips-keymap "u" (lambda () (system "~/quicksnips/bin/quicksnips rhel get-public-ip")))
+(bind-keys quicksnips-keymap "o" (lambda () (system "quicksnips -s server-overview rhel")))
+(bind-keys quicksnips-keymap "u" (lambda () (system "quicksnips -s get-public-ip rhel")))
 (bind-keys quicksnips-keymap "z" '(synthesize-event "C-z" (input-focus)))
 
 (define quicksnips-f-keymap (make-keymap))
 (bind-keys quicksnips-keymap "f" quicksnips-f-keymap)
-(bind-keys quicksnips-f-keymap "b" (lambda () (system "~/quicksnips/bin/quicksnips rhel file-backup")))
+(bind-keys quicksnips-f-keymap "b" (lambda () (system "quicksnips -s file-backup rhel")))
 
 (define quicksnips-a-keymap (make-keymap))
 (bind-keys quicksnips-keymap "a" quicksnips-a-keymap)
-(bind-keys quicksnips-a-keymap "b" (lambda () (system "~/quicksnips/bin/quicksnips rhel apache-buddy")))
-(bind-keys quicksnips-a-keymap "g" (lambda () (system "~/quicksnips/bin/quicksnips rhel apache-log-growers")))
+(bind-keys quicksnips-a-keymap "b" (lambda () (system "quicksnips -s apache-buddy rhel")))
+(bind-keys quicksnips-a-keymap "g" (lambda () (system "quicksnips -s apache-log-growers rhel")))
